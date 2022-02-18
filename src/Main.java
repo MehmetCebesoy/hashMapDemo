c084c0134fd2fb84f7ce0492b440137aa16847e4
Mehmet CEBESOY <mhmtcbsy@gmail.com>
2022-02-19 00:49:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HashMap<String,String> sozluk=new HashMap<String,String>();
+        sozluk.put("book","kitap");
+        sozluk.put("table","masa");
+        sozluk.put("computer","bilgisayar");
+        System.out.println(sozluk);
+        System.out.println(sozluk.size());
+
+        System.out.println(sozluk.get("table"));
+        System.out.println(sozluk.get("masa"));
+
+        for(String item :sozluk.keySet()){
+            System.out.println("Eleman-"+item+" Değer-"+sozluk.get(item));
+
+
+        }
+
+
+    }
+}
